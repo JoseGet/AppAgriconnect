ce3c5ccfd282782a87360f8ddae06215643ceb63
JoseGet <getuliojose96@gmail.com>
2026-02-21 17:33:53 -0400

    Atualizacao de version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.joseGet.careiroapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
